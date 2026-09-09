d863e6d1fa41cc8e7c780b5a327a2556d0f0f6b3
alexey-va <freedeeml@gmail.com>
2026-09-09 21:56:15 +0300

    Adopt Core 2.7.5 automatic native dialog history

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ru.ruscrafting"
-version = "0.1.23"
+version = "0.1.24"
 description = "Rich EcoJobs interface and LuckPerms-backed boosts for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
@@ -39,11 +39,11 @@ kotlin { jvmToolchain(25) }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("ru.ruscrafting.arc:arc-core:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.4")
-    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.4")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.5")
+    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.5")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.willfp:EcoJobs:2026.33")
     compileOnly("com.willfp:eco:2026.33")
@@ -66,11 +66,11 @@ dependencies {
     testImplementation("net.luckperms:api:5.5")
     testImplementation("ru.ruscrafting.thirdparty:rediseconomy:4.5.12")
     testImplementation("me.clip:placeholderapi:2.12.3")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.4")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.5")
     testImplementation("org.yaml:snakeyaml:2.5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.4")
+    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.5")
     configurations["integrationTestImplementation"].extendsFrom(configurations["testImplementation"])
     configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
 }
